7b1417682ebfbfe9a7d9c0d5f8052e76360552c9
Paul Lemus <paullemus@protonmail.com>
2021-03-06 15:51:56 -0800

    org-super-agenda installed. massive update.

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,10 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("01cf34eca93938925143f402c2e6141f03abb341f27d1c2dba3d50af9357ce70" "e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" "5b809c3eae60da2af8a8cfba4e9e04b4d608cb49584cb5998f6e4a1c87c057c4" "f2927d7d87e8207fa9a0a003c0f222d45c948845de162c885bf6ad2a255babfd" "4bca89c1004e24981c840d3a32755bf859a6910c65b829d9441814000cf6c3d0" "6c3b5f4391572c4176908bb30eddc1718344b8eaff50e162e36f271f6de015ca" "7b3d184d2955990e4df1162aeff6bfb4e1c3e822368f0359e15e2974235d9fa8" "93ed23c504b202cf96ee591138b0012c295338f38046a1f3c14522d4a64d7308" "c83c095dd01cde64b631fb0fe5980587deec3834dc55144a6e78ff91ebc80b19" "3c2f28c6ba2ad7373ea4c43f28fcf2eed14818ec9f0659b1c97d4e89c99e091e" "2cdc13ef8c76a22daa0f46370011f54e79bae00d5736340a5ddfe656a767fddf" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "c086fe46209696a2d01752c0216ed72fd6faeabaaaa40db9fc1518abebaf700d" "188fed85e53a774ae62e09ec95d58bb8f54932b3fd77223101d036e3564f9206" "79278310dd6cacf2d2f491063c4ab8b129fee2a498e4c25912ddaa6c3c5b621e" "d5a878172795c45441efcd84b20a14f553e7e96366a163f742b95d65a3f55d71" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" "3df5335c36b40e417fec0392532c1b82b79114a05d5ade62cfe3de63a59bc5c6" "6c9cbcdfd0e373dc30197c5059f79c25c07035ff5d0cc42aa045614d3919dab4" default))
  '(org-agenda-files
-   '("~/Dropbox/org/projects/personal/personal.org" "/home/helpotters/Dropbox/org/todo.org")))
+   '("~/Dropbox/org/projects/personal/personal.org" "/home/helpotters/Dropbox/org/journal.org"))
+ '(org-agenda-time-grid
+   '((daily today require-timed)
+     (800 1000 1200 1400 1600 1800 2000)
+     "      " "     ")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
